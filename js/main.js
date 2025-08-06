6fab6ee93269c9ea6cafdb6e64c17e4b2293974f
hajarzain <hajarzain222@gmail.com>
2025-08-06 19:13:35 +0300

    Upload Files

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.buttons button');
+
+buttons.forEach(button => {
+    const value = button.dataset.value;
+    const action = button.dataset.action;
+
+    button.addEventListener('click', ()=> {
+        if(value !== undefined){
+            const lastChar = display.value.slice(-1);
+            const operator = ['÷', '×', '-', '+', '.'];
+
+            if(operator.includes(value) && operator.includes(lastChar)){
+                return ;
+            }
+
+            display.value += value;
+        }
+
+        else if(action === 'clear'){
+            display.value = '';
+        }
+
+        else if(action === 'backspace'){
+            display.value = display.value.slice(0, -1);
+        }
+
+        else if(action === 'calculate'){
+            try{
+                const expression = display.value
+                .replace(/×/g, '*')
+                .replace(/÷/g, '/');
+                display.value = eval(expression);
+            }
+            catch{
+                display.value = 'Error';
+            }
+        }
+    })
+});
